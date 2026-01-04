d39f64a96867565afffa002acbbfcfab7644164c
skies-starred <skies.starred@gmail.com>
2026-01-04 14:17:14 +0530

    Update SecretHitboxes

---
+++ src/main/java/starred/skies/odin/mixin/blocks/ClipContextMixin.java
@@ -0,0 +1,22 @@
+package starred.skies.odin.mixin.blocks;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.ClipContext;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.VoxelShape;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+import starred.skies.odin.features.impl.cheats.SecretHitboxes;
+
+@Mixin(ClipContext.class)
+public class ClipContextMixin {
+    @Redirect(method = "getBlockShape(Lnet/minecraft/world/level/block/state/BlockState;Lnet/minecraft/world/level/BlockGetter;Lnet/minecraft/core/BlockPos;)Lnet/minecraft/world/phys/shapes/VoxelShape;", at = @At(target = "Lnet/minecraft/world/level/ClipContext$Block;get(Lnet/minecraft/world/level/block/state/BlockState;Lnet/minecraft/world/level/BlockGetter;Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/phys/shapes/CollisionContext;)Lnet/minecraft/world/phys/shapes/VoxelShape;", value = "INVOKE"))
+    VoxelShape getBlockShape(ClipContext.Block instance, BlockState blockState, BlockGetter blockGetter, BlockPos blockPos, CollisionContext collisionContext) {
+        var outline = SecretHitboxes.INSTANCE.getShape(blockState);
+        if (outline != null) return outline;
+        else return instance.get(blockState, blockGetter, blockPos, collisionContext);
+    }
+}
---
+++ src/main/java/starred/skies/odin/mixin/blocks/LevelRendererMixin.java
@@ -0,0 +1,28 @@
+package starred.skies.odin.mixin.blocks;
+
+import net.minecraft.client.renderer.LevelRenderer;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.VoxelShape;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+import starred.skies.odin.features.impl.cheats.SecretHitboxes;
+
+@Mixin(LevelRenderer.class)
+public class LevelRendererMixin {
+    //? if >= 1.21.10 {
+    /*@Redirect(method = "extractBlockOutline(Lnet/minecraft/client/Camera;Lnet/minecraft/client/renderer/state/LevelRenderState;)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/block/state/BlockState;getShape(Lnet/minecraft/world/level/BlockGetter;Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/phys/shapes/CollisionContext;)Lnet/minecraft/world/phys/shapes/VoxelShape;"))
+     *///? } else {
+    @Redirect(method = "renderHitOutline", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/block/state/BlockState;getShape(Lnet/minecraft/world/level/BlockGetter;Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/phys/shapes/CollisionContext;)Lnet/minecraft/world/phys/shapes/VoxelShape;"))
+    //? }
+    VoxelShape extractBlockOutline(BlockState instance, BlockGetter blockGetter, BlockPos blockPos, CollisionContext collisionContext) {
+        var outline = SecretHitboxes.INSTANCE.getShape(instance);
+
+        if (outline != null) return outline;
+        else return instance.getShape(blockGetter, blockPos, collisionContext);
+    }
+}
+
